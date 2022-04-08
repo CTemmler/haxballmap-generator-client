3da0b9867422f3fdb7d39486b04933b097c6e8e8
Constanze Temmler <temmler@4soft.de>
2022-04-08 16:12:34 +0200

    Show fly animation and disable button

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-    import { fly } from 'svelte/transition';
+    import {fly} from 'svelte/transition';
     import type {InputField} from "./types/inputField";
+    import {quintOut} from "svelte/easing";
 
-    let currentVisibleItems = 1;
+    let currentVisibleItems = 0;
+
+    setTimeout(() => {
+        currentVisibleItems += 1;
+    }, 1000)
 
     let stadiumName: string = 'Testname';
     let inputFields: Array<InputField> = [
@@ -20,7 +25,7 @@
         console.log('inputFields', inputFields)
 
         if (index + 1 === currentVisibleItems) {
-              currentVisibleItems += 1;
+            currentVisibleItems += 1;
         }
     }
 
@@ -33,13 +38,15 @@
 <form>
     {#each inputFields as {label, type, value, id}, index(id)}
         {#if index < currentVisibleItems}
-        <div transition:fly>
-            <div><label for={id}>{label}:</label></div>
-            <input {id} name={id} {type} value={value} on:change={e => handleChange(e.target.value, index)}/>
-        </div>
+            <div transition:fly={{delay: 250, duration: 1000, x: 0, y: 500, opacity: 0.5, easing: quintOut}}>
+                <div><label for={id}>{label}:</label></div>
+                <input {id} name={id} {type} value={value} on:change={e => handleChange(e.target.value, index)}/>
+            </div>
         {/if}
     {/each}
-    <input class="form-sent-button" type="button" value="Absenden" on:click={submit3}/>
+    {#if currentVisibleItems === inputFields.length}
+        <input class="form-sent-button" type="button" value="Absenden" on:click={submit3}/>
+    {/if}
 </form>
 
 <style>
@@ -53,7 +60,7 @@
     form > div {
         padding: 5px;
         width: 100%
-        }
+    }
 
     label {
         width: 150px;
