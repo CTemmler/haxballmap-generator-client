cb663d90be863ce9e8b3b3a657f0fe9e69d6244b
Constanze Temmler <temmler@4soft.de>
2022-04-08 15:04:54 +0200

    Created a submit method and fixed stuff
    Also added the best possible design

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -5,20 +5,37 @@ let stadiumName: string = 'Testname';
 let inputFields: Array<InputField> = [
 
     {label:"Stadien Name", type:"string", value:"stadiumName", id:"stadiumName"},
-    {label:"Breite", type:"number", value:"width", id:"width"},
-    {label:"Höhe", type:"number", value:"height", id:"height"},
-    {label:"Spawn-Entfernung", type:"number", value:"spawnDistance", id:"spawnDistance"},
+    {label:"Breite", type:"number", value:9, id:"width"},
+    {label:"Höhe", type:"number", value:9, id:"height"},
+    {label:"Spawn-Entfernung", type:"number", value:9, id:"spawnDistance"},
     {label:"Hintergrund-Typ", type:"string", value:"backgroundType", id:"backgroundType"},
-    {label:"KickOff-Radium", type:"number", value:"kickOffRadius", id:"kickOffRadius"}
+    {label:"KickOff-Radium", type:"number", value:9, id:"kickOffRadius"}
 ]
 
+function handleChange(value: number | string, index: number) {
+    inputFields[index].value = value;
+    console.log('value', value)
+    console.log('inputFields', inputFields)
+}
+
+function submit3()  {
+    console.log('Final Fields: ', inputFields);
+}
 
 </script>
 
-<form method="POST">
-    {#each inputFields as {label, type, value, id}}
+<form>
+    {#each inputFields as {label, type, value, id}, index(id)}
         <label for={id}>{label}:</label>
-        <input {id} name={id} {type} />
+        <input {id} name={id} {type} value={value} on:change={e => handleChange(e.target.value, index)}/>
+        <br>
     {/each}
-    <input type="submit" value="Absenden" />
+    <input type="button" value="Absenden" on:click={submit3} />
 </form>
+
+<style>
+    form {
+        background-color: #f00;
+        color: #0066ff;
+    }
+</style>
