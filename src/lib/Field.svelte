93c1e6531c7b43a223ded61ce5d597604b35e3a0
Constanze Temmler <temmler@4soft.de>
2022-05-06 15:21:25 +0200

    Added kick off radius and players to the drawing

--- src/lib/Field.svelte
+++ src/lib/Field.svelte
@@ -5,9 +5,12 @@
 
     $: fieldWidth = Number(inputFields.find(value => value.id === "width").value);
     $: fieldHeight = Number(inputFields.find(value => value.id === "height").value);
+    $: kickOffRadius = Number(inputFields.find(value => value.id === "kickOffRadius").value);
+    $: spawnDistance = Number(inputFields.find(value => value.id === "spawnDistance").value);
     const lineThickness = 5;
     const offsetX = 50;
     const offsetY = 10;
+    const playerSize = 25;
 
     $: render = ({context}) => {
         context.fillStyle = `hsl(${$t / 40}, 100%, 50%)`;
@@ -17,6 +20,19 @@
         context.rect((fieldWidth / 2) + offsetX, offsetY, 1, fieldHeight + lineThickness);
         context.rect(fieldWidth + offsetX, offsetY, lineThickness, fieldHeight + lineThickness);
         context.rect(offsetX, fieldHeight + offsetY, fieldWidth, lineThickness);
+        context.fill();
+
+        context.strokeStyle = `hsl(${$t / 40}, 100%, 50%)`;
+        context.lineWidth = lineThickness;
+        context.beginPath();
+        context.arc((fieldWidth / 2) + offsetX, (fieldHeight / 2) + offsetY, kickOffRadius, 0, Math.PI*2);
+        context.stroke();
+
+        context.fillStyle = `hsl(${$t / 40}, 100%, 50%)`;
+        context.lineWidth = lineThickness;
+        context.beginPath();
+        context.arc((fieldWidth / 2 - spawnDistance) + offsetX, (fieldHeight / 2) + offsetY, playerSize, 0, Math.PI*2);
+        context.arc((fieldWidth / 2 + spawnDistance) + offsetX, (fieldHeight / 2) + offsetY, playerSize, 0, Math.PI*2);
         context.fill();
     };
 
