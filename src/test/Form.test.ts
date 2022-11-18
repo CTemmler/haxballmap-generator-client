5e8a1dfbfad8376abac71bcb83c672e351bc13cc
Constanze Temmler <temmler@4soft.de>
2022-11-18 15:57:02 +0100

    Further testing

--- src/test/Form.test.ts
+++ src/test/Form.test.ts
@@ -1,15 +1,15 @@
 import {render, screen} from '@testing-library/svelte'
-import {describe, test, vi} from "vitest";
+import {describe, test, vi, beforeEach, afterEach} from "vitest";
 import Form from '../lib/Form.svelte'
 import type {InputField} from "../lib/types/inputField";
 
-describe('Form', ()=> {
+describe('Form', () => {
 
-     beforeEach(() => {
-       vi.useFakeTimers()
-     })
+    beforeEach(() => {
+       vi.useFakeTimers();
+    })
 
-    test('second field should be displayed', () => {
+    test('second field should be displayed',  () => {
         const inputFields: Array<InputField> = [{
             label: 'Field 1',
             id: 'field1',
@@ -22,9 +22,9 @@ describe('Form', ()=> {
             value: undefined
         }];
         render(Form, {inputFields});
-        vi.advanceTimersByTime(2000)
-            const heading = screen.getByTestId('field1');
-            console.log('heading', heading);
+        vi.advanceTimersByTime(2000);
+        const heading = screen.getByTestId('field1');
+        console.log('heading', heading);
         // const inputField1BeforeValueInField1 = screen.getByText(inputFields[0].label);
         // const inputField2BeforeValueInField1 = screen.getByTestId(inputFields[2].id);
 
@@ -32,9 +32,9 @@ describe('Form', ()=> {
 
     })
 
-     afterEach(() => {
-       vi.useRealTimers()
-     })
+    afterEach(() => {
+        vi.useRealTimers()
+    })
 
 })
 
